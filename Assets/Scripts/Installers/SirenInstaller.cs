51cca5388a3b293c9b9b44193f4727333161dd76
MisterioRemo <misterioremo@gmail.com>
2024-04-13 10:26:10 +0300

    Добавлен SirenInstaller

---
+++ Assets/Scripts/Installers/SirenInstaller.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using Zenject;
+
+namespace aprilJam
+{
+  public class SirenInstaller : MonoInstaller
+  {
+    [SerializeField] private GameObject siren;
+
+    public override void InstallBindings()
+    {
+      Container
+        .Bind<SirenSong>()
+        .FromNewComponentOn(siren)
+        .AsSingle()
+        .NonLazy();
+
+      Container
+        .BindInterfacesAndSelfTo<NoteCombination>()
+        .FromNew()
+        .AsSingle()
+        .NonLazy();
+    }
+  }
+}
---
+++ Assets/Scripts/Notes/NoteCombination.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace aprilJam
+{
+  public class NoteCombination
+  {
+    #region PARAMETERS
+    private Dictionary<string, Action> combinations;
+    #endregion
+
+
+  }
+}
